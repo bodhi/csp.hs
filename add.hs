abf0551ebf62b034533b57756ef5a4922a3a4f24
bodhi <bodhi@5263.org>
2015-04-20 00:16:32 +1000

    Clean up add.hs

--- add.hs
+++ add.hs
@@ -9,52 +9,35 @@ type Eqn = Game Alphabet Int
 
 type EqnConstraint = Constraint Alphabet Int
 
-eqn :: [Int] -> Bool
-eqn (a:b:c:carry:[]) = a + b == c + 10 * carry
-        
-eqn2 (s:e:n:d:m:o:r:y:[]) =
-                (s * 1000 + e * 100 + n * 10 + d)
-              + (m * 1000 + o * 100 + r * 10 + e)
- == (m * 10000 + o * 1000 + n * 100 + e * 10 + y)
-
-
 --   SEND
 -- + MORE
 ---------
--- =MONEY  
+-- =MONEY
 constraints :: [EqnConstraint]
 constraints = firstDigit:alldiff:digitConstraints
 
-xxx :: [(Alphabet,Int)] -> Bool
-xxx ((Null,a):(Null,b):(_,x0):(_,r):(_,_):[]) = x0 == r
-xxx ((va,a):(vb,b):(_,x0):(vr,r):(_,x1):[]) = a + b + x0 == r + 10 * x1 &&
+addColumn :: [(Alphabet,Int)] -> Bool
+addColumn ((Null,a):(Null,b):(_,x0):(_,r):(_,_):[]) = x0 == r
+addColumn ((va,a):(vb,b):(_,x0):(vr,r):(_,x1):[]) = a + b + x0 == r + 10 * x1 &&
                                               (va == vb || a /= b) &&
                                               (va == vr || a /= r) &&
                                               (vb == vr || b /= r)
-
--- digitConstraints =
---   let tuples = [[D, E, Y, X1],
---                 [N, R, E, X2],
---                 [E, O, N],
---                 [S, M, O]]
---   in map (\vars -> KConstraint vars eqn) tuples
---digitConstraints :: [EqnConstraint]
 
 --   TWO
 --  +TWO
 --------
 -- =FOUR
 digitConstraints = [
-  -- KConstraint (O:O:(Carry 0):R:(Carry 1):[]) xxx,
-  -- KConstraint (W:W:(Carry 1):U:(Carry 2):[]) xxx,
-  -- KConstraint (T:T:(Carry 2):O:(Carry 3):[]) xxx,
-  -- KConstraint (Null:Null:(Carry 3):F:(Carry 4):[]) xxx
+  -- KConstraint (O:O:(Carry 0):R:(Carry 1):[]) addColumn,
+  -- KConstraint (W:W:(Carry 1):U:(Carry 2):[]) addColumn,
+  -- KConstraint (T:T:(Carry 2):O:(Carry 3):[]) addColumn,
+  -- KConstraint (Null:Null:(Carry 3):F:(Carry 4):[]) addColumn
 
-  KVarConstraint (D:E:(Carry 0):Y:(Carry 1):[]) xxx,
-  KVarConstraint (N:R:(Carry 1):E:(Carry 2):[]) xxx,
-  KVarConstraint (E:O:(Carry 2):N:(Carry 3):[]) xxx,
-  KVarConstraint (S:M:(Carry 3):O:(Carry 4):[]) xxx,
-  KVarConstraint (Null:Null:(Carry 4):M:(Carry 5):[]) xxx  
+  KVarConstraint (D:E:(Carry 0):Y:(Carry 1):[]) addColumn,
+  KVarConstraint (N:R:(Carry 1):E:(Carry 2):[]) addColumn,
+  KVarConstraint (E:O:(Carry 2):N:(Carry 3):[]) addColumn,
+  KVarConstraint (S:M:(Carry 3):O:(Carry 4):[]) addColumn,
+  KVarConstraint (Null:Null:(Carry 4):M:(Carry 5):[]) addColumn
   ]
 
 
@@ -65,9 +48,8 @@ alldiff :: EqnConstraint
 alldiff = Alldiff alphabet
 
 firstDigit :: EqnConstraint
+-- Poor man's node constraint
 firstDigit = ArcConstraint M Null (\(m:null) -> m > 0)
-
---sumConstraint = KConstraint "SENDMORY" eqn2
 
 carries = (Carry 0,[0]) : (map (flip (,) [0,1]) $ map Carry [1..5])
 
