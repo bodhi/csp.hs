36649e634c4107561b2ed015e0b16b73768ae680
bodhi <bodhi@5263.org>
2015-04-22 00:35:03 +1000

    Implement decoding cryptarithemtic problems dynamically

--- add.hs
+++ add.hs
@@ -1,20 +1,19 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 import CSP
 
+import qualified Data.List as L
 import qualified Data.Set as Set
 import qualified Data.Map as Map
+import Data.Data
 
-data Alphabet = S | E | N | D | M | O | R | Y | T | W | F | U | Null | Carry Int deriving (Eq, Ord, Show)
+data Alphabet = A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q |R | S | T | U | V | W | X | Y | Z | Null | Carry Int deriving (Eq, Ord, Show, Typeable, Data)
 
 type Eqn = Game Alphabet Int
 
 type EqnConstraint = Constraint Alphabet Int
 
---   SEND
--- + MORE
----------
--- =MONEY
-constraints :: [EqnConstraint]
-constraints = firstDigit:alldiff:digitConstraints
+--------------------
 
 addColumn :: [(Alphabet,Int)] -> Bool
 addColumn ((Null,a):(Null,b):(_,x0):(_,r):(_,_):[]) = x0 == r
@@ -23,40 +22,59 @@ addColumn ((va,a):(vb,b):(_,x0):(vr,r):(_,x1):[]) = a + b + x0 == r + 10 * x1 &&
                                               (va == vr || a /= r) &&
                                               (vb == vr || b /= r)
 
---   TWO
---  +TWO
---------
--- =FOUR
-digitConstraints = [
-  -- KConstraint (O:O:(Carry 0):R:(Carry 1):[]) addColumn,
-  -- KConstraint (W:W:(Carry 1):U:(Carry 2):[]) addColumn,
-  -- KConstraint (T:T:(Carry 2):O:(Carry 3):[]) addColumn,
-  -- KConstraint (Null:Null:(Carry 3):F:(Carry 4):[]) addColumn
+--------------------
 
-  KVarConstraint (D:E:(Carry 0):Y:(Carry 1):[]) addColumn,
-  KVarConstraint (N:R:(Carry 1):E:(Carry 2):[]) addColumn,
-  KVarConstraint (E:O:(Carry 2):N:(Carry 3):[]) addColumn,
-  KVarConstraint (S:M:(Carry 3):O:(Carry 4):[]) addColumn,
-  KVarConstraint (Null:Null:(Carry 4):M:(Carry 5):[]) addColumn
-  ]
+columnConstraints :: [Alphabet] -> [Alphabet] -> [Alphabet] -> [EqnConstraint]
+columnConstraints a b c =
+  let toConstraint = \a b c col -> columnConstraint col (a,b) c
+      (a':b':c':[]) = map reverse [a, b, c]
+  in reverse $ L.zipWith4 toConstraint a' b' c' [0..]
 
+--  a
+-- +b
+-----
+-- =c
+columnConstraint :: Int -> (Alphabet,Alphabet) -> Alphabet -> EqnConstraint
+columnConstraint column (a, b) c = KVarConstraint [a, b, (Carry column), c, (Carry $ column + 1)] addColumn
 
-alphabet :: [Alphabet]
-alphabet = M:S:E:N:D:O:R:Y:[]
-
-alldiff :: EqnConstraint
-alldiff = Alldiff alphabet
-
-firstDigit :: EqnConstraint
+firstDigit :: Alphabet -> EqnConstraint
 -- Poor man's node constraint
-firstDigit = ArcConstraint M Null (\(m:null) -> m > 0)
+firstDigit a = ArcConstraint a Null (\(m:null) -> m > 0)
 
-carries = (Carry 0,[0]) : (map (flip (,) [0,1]) $ map Carry [1..5])
+--------------------
 
-alphaDomain = map (flip (,) [0..9]) (drop 0 alphabet)
+carries n = (Carry 0,[0]) : (map (flip (,) [0,1]) $ map Carry [1..n])
 
-game :: Eqn
-game = let eqnData = (Null,[0]) : (alphaDomain ++ carries)
+alphaDomain alphabet = map (flip (,) [0..9]) alphabet
+
+game :: [Alphabet] -> Int -> Eqn
+game alphabet columns = let eqnData = (Null,[0]) : (alphaDomain alphabet ++ carries columns)
        in makeGame eqnData
 
-main = putStrLn $ show $ solve constraints game
+--------------------
+
+decodeChar :: Char -> Maybe Alphabet
+decodeChar c = fromConstr <$> readConstr (dataTypeOf Null) [c]
+
+decode :: String -> Maybe [Alphabet]
+decode s = sequence $ map decodeChar s
+
+fillLeft :: Int -> [Alphabet] -> [Alphabet]
+fillLeft len a
+  | toFill > 0 = replicate toFill Null ++ a
+  | otherwise = a
+  where toFill = len - (length a)
+
+mkGame :: [[Alphabet]] -> (Eqn, [EqnConstraint])
+mkGame (a:b:c:[]) =
+  let len = length c
+      alphabet = a ++ b ++ c
+      (a':b':[]) = map (fillLeft len) [a,b]
+  in (game alphabet len, Alldiff alphabet:firstDigit (head c):columnConstraints a' b' c)
+
+decodeGame :: String -> String -> String -> Maybe (Eqn, [EqnConstraint])
+decodeGame a b c = do sum <- sequence $ map decode [a,b,c]
+                      pure $ mkGame sum
+
+main = let Just (game,constraints) = decodeGame "SEND" "MORE" "MONEY"
+       in putStrLn $ show $ solve constraints game
